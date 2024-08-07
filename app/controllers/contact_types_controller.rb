aeeab7f6babb6ac3844a980b2c56d0aea11b7649
mulenokv <mulenokivan@mail.ru>
2024-08-07 13:16:32 +0700

    cleanup controllers&&gemfile

--- app/controllers/contact_types_controller.rb
+++ app/controllers/contact_types_controller.rb
@@ -1,52 +1,40 @@
 class ContactTypesController < ApplicationController
   before_action :set_contact_type, only: %i[ show update destroy ]
 
-  # GET /contact_types
-  # GET /contact_types.json
   def index
     @contact_types = ContactType.all
   end
 
-  # GET /contact_types/1
-  # GET /contact_types/1.json
   def show
   end
 
-  # POST /contact_types
-  # POST /contact_types.json
   def create
     @contact_type = ContactType.new(contact_type_params)
 
     if @contact_type.save
-      render :show, status: :created, location: @contact_type
+      render :show, status: :created
     else
       render json: @contact_type.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /contact_types/1
-  # PATCH/PUT /contact_types/1.json
   def update
     if @contact_type.update(contact_type_params)
-      render :show, status: :ok, location: @contact_type
+      render :show, status: :ok
     else
       render json: @contact_type.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /contact_types/1
-  # DELETE /contact_types/1.json
   def destroy
     @contact_type.destroy!
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_contact_type
       @contact_type = ContactType.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def contact_type_params
       params.require(:contact_type).permit(:title)
     end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,52 +1,40 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: %i[ show update destroy ]
 
-  # GET /profiles
-  # GET /profiles.json
   def index
     @profiles = Profile.all
   end
 
-  # GET /profiles/1
-  # GET /profiles/1.json
   def show
   end
 
-  # POST /profiles
-  # POST /profiles.json
   def create
     @profile = Profile.new(profile_params)
 
     if @profile.save
-      render :show, status: :created, location: @profile
+      render :show, status: :created
     else
       render json: @profile.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /profiles/1
-  # PATCH/PUT /profiles/1.json
   def update
     if @profile.update(profile_params)
-      render :show, status: :ok, location: @profile
+      render :show, status: :ok
     else
       render json: @profile.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /profiles/1
-  # DELETE /profiles/1.json
   def destroy
     @profile.destroy!
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_profile
       @profile = Profile.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def profile_params
       params.require(:profile).permit(:name, :status, :avatar)
     end
